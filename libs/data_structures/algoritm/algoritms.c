54784d199a9468ee2b7c830880d583f182213cd9
unknown <vlad.yegorov200524@mail.ru>
2024-04-28 12:26:20 +0300

    task 2

--- libs/data_structures/algoritm/algoritms.c
+++ libs/data_structures/algoritm/algoritms.c
@@ -24,5 +24,14 @@ void sortRowsByMaxElem(matrix *m) {
     insertionSortRowsMatrixByRowCriteria(*m, getMax);
 }
 
+int getMin(int *a, const int n) {
+    int min = a[0];for (int i = 0; i < n; i++)
+        if (a[i] < min)
+            min = a[i];
+    return min;
+}
+void sortColsByMinElem(matrix *m) {
+    selectionSortColsMatrixByColCriteria(*m, getMin);
+}
 
 
