b90b66cea1a5a3536f39e028af7bdd71d6ee6c4e
unknown <vlad.yegorov200524@mail.ru>
2024-04-28 12:29:14 +0300

    task 6

--- libs/data_structures/algoritm/algoritms.c
+++ libs/data_structures/algoritm/algoritms.c
@@ -79,4 +79,11 @@ void transposeIfMatrixHasNotEqualSumOfRows(matrix *m) {long long sum[m->nRows];
         transposeSquareMatrix(m);
 }
 
+bool isMutuallyInverseMatrices(matrix m1, matrix m2) {
+    matrix multiplication = mulMatrices(m1, m2);
+    if (isEMatrix(&multiplication))
+        return true;
+    return false;
+}
+
 
